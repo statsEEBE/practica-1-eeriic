9eea18e00bca930dbdcfb7f1725683ebe9573b09
eric.cabrera <eric.cabrera@auleseebe.upc.edu>
2024-09-19 13:41:33 +0200

    p3

--- Problema3.R
+++ Problema3.R
@@ -1,1 +1,14 @@
 #Codigo para problema 3
+x <- (1:30)/30
+x
+#a) suma de todos los datos
+sum(x)
+#b) crea un vector Y a partir de X eliminando los datos de: 21, 2, 26
+y <- x[-c(21,2,26)]
+y
+#me pide calcular con exponencial la resta de xi y yi
+sum(exp(x))- sum (exp(y))
+#c) en el vector Y, el dato de la posicion 19, es mayor o igual q el de la 24?
+y[19]
+y[24]
+#falso
